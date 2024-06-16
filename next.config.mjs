36263d31cc1f0bd926ffe5ef9cb7d1d51c611b4d
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-06-16 11:19:44 -0400

    build(*): Edit config files for Next, TS, ESLint, PostCSS and TailwindCSS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+  reactStrictMode: true, // Essential to catch potential problems early
+  swcMinify: true, // Faster builds and smaller production builds
+  experimental: {
+    scrollRestoration: true, // Restores scroll position during navigation
+  },
+  images: { // Crucial to utilize image optimization capabilities for format like AVF or WebP
+    domains: [], 
+    formats: ['image/avif', 'image/webp'],
+  },
+  webpack: (config, { isServer }) => { // Adds support for features useful in client-side only environments
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+      };
+    }
+    return config;
+  },
+  env: {
+    CUSTOM_API_URL: process.env.CUSTOM_API_URL, // Exposes environment variables to securely access configuration data
+  },
+};
