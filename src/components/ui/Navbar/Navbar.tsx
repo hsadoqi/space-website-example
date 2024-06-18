03084d7fe656d22c03b22b9bd8c70f87fe5bd9c9
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-06-18 14:52:17 -0400

    feat(nav): Add links to Navbar for sections

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -1,9 +1,30 @@
 import React, { FC } from 'react';
+import Link from 'next/link'
+
+const NavbarItem: FC<{item: string}> = ({ item }) => {
+    return (
+        <Link
+            href={`#${item.toLowerCase()}`}
+            className="p-2"
+            >
+                {item}
+            </Link>
+    )
+}
 
 const Navbar: FC = () => {
-    return (
-        <nav>
+    const items = [
+        "About", 
+        "Skills", 
+        "Experience", 
+        "Projects", 
+        "Writing", 
+        "Contact"
+    ]
 
+    return (
+        <nav className="flex justify-center">
+            {items.map(item => <NavbarItem item={item} />)}
         </nav>
     )
 }
