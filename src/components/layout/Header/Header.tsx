c88ea1d0d26480cb9d3e71928fd59aa16156183d
Hanaa Sadoqi <hsadoqi@gmail.com>
2024-06-20 01:53:53 -0400

    feat(navigation): Implement scrolling behavior for section navigation

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -4,77 +4,85 @@ import React, { FC, useEffect, useState } from 'react';
 import { Navbar } from '../../ui';
 import clsx from 'clsx';
 
+const items = [
+  "About", 
+  "Skills", 
+  "Experience", 
+  "Projects", 
+  "Writing", 
+  "Contact"
+];
 
 const Header: FC = () => {
-    const [isHeaderVisible, setIsHeaderVisible] = useState(false);
+    const [isHeaderVisible, setIsHeaderVisible] = useState(true);
     const [lastScrollY, setLastScrollY] = useState(0);
+    const [isHovering, setIsHovering] = useState(false);
 
     useEffect(() => {
-        const handleScroll = () => {      
-            // Detect if user is scrolling up
+        const handleScroll = () => {
             if (window.scrollY < lastScrollY || window.scrollY === 0) {
-              setIsHeaderVisible(true);
-
-              setTimeout(() => {
+                setIsHeaderVisible(true);
+            } else if(!isHovering) {
                 setIsHeaderVisible(false);
-              }, 2000)
-            } else {
-              setIsHeaderVisible(false);
             }
             setLastScrollY(window.scrollY);
-          };
-      
-          // Add event listener to viewport
-          window.addEventListener('scroll', handleScroll);
-      
-          // Clean up event listener on component unmount
-          return () => window.removeEventListener('scroll', handleScroll);
-    }, [lastScrollY]);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [lastScrollY, isHovering]);
 
     useEffect(() => {
-        // Show header when user hovers over area
-        const handleMouseEnter = () => {
-          setIsHeaderVisible(true);
-        };
-    
-        // Hide header when user leaves area 
-        // if they are not scrolled to the top of the page
-        const handleMouseLeave = () => {
-          if (window.scrollY > 0) {
-            setIsHeaderVisible(false);
-          }
-        };
-        
-        const detectionArea = document.getElementById('header-detection-area');
+      const handleMouseEnter = () => {
+        setIsHovering(true);
+        setIsHeaderVisible(true);
+      };
 
-        detectionArea?.addEventListener('mouseenter', handleMouseEnter);
-        detectionArea?.addEventListener('mouseleave', handleMouseLeave);
-    
-        return () => {
-          detectionArea?.removeEventListener('mouseenter', handleMouseEnter);
-          detectionArea?.removeEventListener('mouseleave', handleMouseLeave);
-        };
+      const handleMouseLeave = () => {
+        setIsHovering(false);
+        if( window.scrollY > 0) {
+          setIsHeaderVisible(false);
+        }
+      };
+
+      const detectionArea = document.getElementById('header-detection-area');
+      const header = document.getElementById('header');
+
+      detectionArea?.addEventListener('mouseenter', handleMouseEnter);
+      detectionArea?.addEventListener('mouseleave', handleMouseLeave);
+      header?.addEventListener('mouseenter', handleMouseEnter);
+      header?.addEventListener('mouseleave', handleMouseLeave);
+      
+      return () => {
+        detectionArea?.removeEventListener('mouseenter', handleMouseEnter);
+        detectionArea?.removeEventListener('mouseleave', handleMouseLeave);
+        header?.removeEventListener('mouseenter', handleMouseEnter);
+        header?.removeEventListener('mouseleave', handleMouseLeave);
+      };
     }, []);
 
     return (
         <>
-            <div 
-              id="header-detection-area"
-              className="fixed top-0 left-0 w-full h-16 z-40 pointer-events-auto"
+            <div
+                id="header-detection-area"
+                className="fixed top-0 left-0 w-full h-16 z-40 pointer-events-auto"
             ></div>
             <header
-              className={clsx(
-                'fixed top-0 left-0 w-full p-6 bg-white shadow transition-transform duration-500 z-50',
-                {
-                  'translate-y-0': isHeaderVisible,
-                  '-translate-y-full': !isHeaderVisible,
-                }
-              )}
+                id="header"
+                className={clsx(
+                    'fixed top-0 left-0 w-full bg-white shadow transition-transform duration-500 z-50',
+                    {
+                        'translate-y-0': isHeaderVisible,
+                        '-translate-y-full': !isHeaderVisible,
+                    }
+                )}
             >
-                <Navbar />
+                <Navbar items={items} />
             </header>
         </>
-    )
-}
+    );
+};
 
 export default Header;
+
--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -1,30 +1,48 @@
-import React, { FC } from 'react';
-import Link from 'next/link'
+'use client'
 
-const NavbarItem: FC<{item: string}> = ({ item }) => {
+import React, { FC, useState } from 'react';
+import Link from 'next/link'
+import Logo from '../Logo';
+import clsx from 'clsx'
+
+export const NavbarItem: FC<{item: string}> = ({ item }) => {
+    const [isHovered, setIsHovered] = useState(false);
+
     return (
-        <Link
-            href={`#${item.toLowerCase()}`}
-            className="p-2"
-            >
+        <li className={clsx(
+            'text-base font-medium p-2',
+            {
+                'ring ring-offset-1 ring-cyan-500 rounded-lg': isHovered,
+                'ring-0': !isHovered,
+            }
+        )}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
+            <Link
+                href={`#${item.toLowerCase()}`}
+                >
                 {item}
             </Link>
+        </li>
     )
 }
 
-const Navbar: FC = () => {
-    const items = [
-        "About", 
-        "Skills", 
-        "Experience", 
-        "Projects", 
-        "Writing", 
-        "Contact"
-    ]
-
+const Navbar: FC<{items?: string[]}> = ({ items = [] }) => {
     return (
-        <nav className="flex justify-center">
-            {items.map(item => <NavbarItem item={item} />)}
+        <nav className="flex justify-between items-center shadow-md pr-12">
+            <div className="flex items-center">
+                <Link href="/" className="flex items-center">
+                    <Logo />
+                    <span className="sr-only">Hanaa Sadoqi</span>
+                </Link>
+            </div>
+            <ul className="hidden lg:flex lg:justify-center lg:gap-x-4">
+                {items.map(item => <NavbarItem key={item} item={item} />)}
+            </ul>
+            <div className="h-12 w-12 hamburger-menu">
+                <h1>Hello</h1>
+            </div>
         </nav>
     )
 }
